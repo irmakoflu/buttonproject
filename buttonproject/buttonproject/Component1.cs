f7ba70b1a96d90d41fb9277a1708db57e541c25d
irmakoflu <irmakoflu@icloud.com>
2024-12-08 16:11:22 +0300

    buttondrawing

---
+++ buttonproject/buttonproject/Component1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buttonproject
+{
+    public partial class Component1 : Button
+    {
+        public Component1()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.ForeColor=Color.White;
+            this.BackColor=Color.Orange;
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+           // Graphics graphics=this.CreateGraphics();
+            SolidBrush solidBrush=new SolidBrush(Color.White);
+            Pen pen =new Pen (solidBrush);
+            pevent.Graphics.DrawLine(pen,new Point(5,5),new Point(this.Width-5,5));
+            pevent.Graphics.DrawLine(pen,new Point(5,this.Height-5),new Point(this.Width-5, this.Height - 5));
+            pevent.Graphics.DrawLine(pen,new Point(5,5),new Point(5, this.Height - 5));
+            pevent.Graphics.DrawLine(pen,new Point(this.Width-5,5),new Point(this.Width-5,this.Height-5));
+        }
+
+
+    }
+}
---
+++ buttonproject/buttonproject/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace buttonproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            component11 = new Component1();
+            SuspendLayout();
+            // 
+            // component11
+            // 
+            component11.BackColor = Color.Orange;
+            component11.FlatAppearance.BorderSize = 0;
+            component11.FlatStyle = FlatStyle.Flat;
+            component11.ForeColor = Color.White;
+            component11.Location = new Point(238, 94);
+            component11.Name = "component11";
+            component11.Size = new Size(240, 127);
+            component11.TabIndex = 0;
+            component11.Text = "component11";
+            component11.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(component11);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Component1 component11;
+    }
+}
